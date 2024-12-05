6ecd4f050e5f8ef0729311c689521b4abc7bc822
ChumboMJ <tforste@gmail.com>
2024-12-04 16:19:16 -0800

    Formatting in the comments

--- SecretMessageCodingChallenge.py
+++ SecretMessageCodingChallenge.py
@@ -1,8 +1,12 @@
 # Coding Exercise: Decoding a Secret Message
-# In this exercise, you will write code to solve a problem. Your code must be in either Python or JavaScript—solutions in other languages will not be accepted! You can write your code using any IDE you want.
+# In this exercise, you will write code to solve a problem. Your code must be in either Python or JavaScript—solutions in other 
+# languages will not be accepted! You can write your code using any IDE you want.
 
 # Problem
-# You are given a Google Doc like this one that contains a list of Unicode characters and their positions in a 2D grid. Your task is to write a function that takes in the URL for such a Google Doc as an argument, retrieves and parses the data in the document, and prints the grid of characters. When printed in a fixed-width font, the characters in the grid will form a graphic showing a sequence of uppercase letters, which is the secret message.
+# You are given a Google Doc like this one that contains a list of Unicode characters and their positions in a 2D grid. 
+# Your task is to write a function that takes in the URL for such a Google Doc as an argument, retrieves and parses the data in 
+# the document, and prints the grid of characters. When printed in a fixed-width font, the characters in the grid will form a 
+# graphic showing a sequence of uppercase letters, which is the secret message.
 # The document specifies the Unicode characters in the grid, along with the x- and y-coordinates of each character.
 # The minimum possible value of these coordinates is 0. There is no maximum possible value, so the grid can be arbitrarily large.
 # Any positions in the grid that do not have a specified character should be filled with a space character.
@@ -39,5 +43,6 @@ def process_google_doc(url):
 
 
 # Example usage
-google_doc_url = "https://docs.google.com/document/d/your_document_id/export?format=txt"
-process_google_doc(google_doc_url)
+simple_google_doc_url = "https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub"
+google_doc_url = "https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5ieJuYwpt7YHYEzeNJkIb9OSDdx-ov2nRNReKQyey-cwJOoEKUhLmN9z/pub"
+process_google_doc(simple_google_doc_url)
