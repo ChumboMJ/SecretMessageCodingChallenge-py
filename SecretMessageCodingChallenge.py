d5c367263014bc4d00e9d880226b599c8ecef05b
ChumboMJ <tforste@gmail.com>
2024-12-09 15:20:27 -0800

    Remove try/catch block

--- SecretMessageCodingChallenge.py
+++ SecretMessageCodingChallenge.py
@@ -30,35 +30,30 @@ import pandas as pd
 import lxml
 
 def process_google_doc(url):
-    try:
-        df = pd.read_html(url, header=0, encoding='utf-8')
-        table = df[0]
+    df = pd.read_html(url, header=0, encoding='utf-8')
+    table = df[0]
 
-        x_coordinates = table["x-coordinate"]
-        y_coordinates = table["x-coordinate"]
+    x_coordinates = table["x-coordinate"]
+    y_coordinates = table["x-coordinate"]
 
-        # Determine Grid Size for output
-        max_x = x_coordinates.max()
-        max_y = y_coordinates.max()
+    # Determine Grid Size for output
+    max_x = x_coordinates.max()
+    max_y = y_coordinates.max()
 
-        # Create an empty grid
-        output_grid = [[' ' for _ in range(max_y + 1)] for _ in range(max_x + 1)]
+    # Create an empty grid
+    output_grid = [[' ' for _ in range(max_y + 1)] for _ in range(max_x + 1)]
 
-        # Place characters in the grid
-        for index, row in table.iterrows():
-            x, y, char = row["x-coordinate"], row["y-coordinate"], row["Character"]
-            output_grid[x][y] = char
+    # Place characters in the grid
+    for index, row in table.iterrows():
+        x, y, char = row["x-coordinate"], row["y-coordinate"], row["Character"]
+        output_grid[x][y] = char
 
-        for row in output_grid:
-            print(''.join(row))
-
-    except requests.exceptions.RequestException as ex:
-        print(f"An error with the request has occurred: {ex}")
-        return None
-
+    for row in output_grid:
+        print(''.join(row))
 
 # Example usage
 simple_google_doc_url = "https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub"
 google_doc_url = "https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5ieJuYwpt7YHYEzeNJkIb9OSDdx-ov2nRNReKQyey-cwJOoEKUhLmN9z/pub"
 process_google_doc(simple_google_doc_url)
 process_google_doc(google_doc_url)
+process_google_doc("this is not a real url")
