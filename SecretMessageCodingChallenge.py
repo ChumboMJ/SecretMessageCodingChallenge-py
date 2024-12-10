b40728519d775da681d548d0d1d875eb321af214
ChumboMJ <tforste@gmail.com>
2024-12-09 20:09:13 -0800

    Comment out the full doc url until the simple doc is working

--- SecretMessageCodingChallenge.py
+++ SecretMessageCodingChallenge.py
@@ -55,4 +55,4 @@ def process_google_doc(url):
 simple_google_doc_url = "https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub"
 google_doc_url = "https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5ieJuYwpt7YHYEzeNJkIb9OSDdx-ov2nRNReKQyey-cwJOoEKUhLmN9z/pub"
 process_google_doc(simple_google_doc_url)
-process_google_doc(google_doc_url)
+# process_google_doc(google_doc_url)
