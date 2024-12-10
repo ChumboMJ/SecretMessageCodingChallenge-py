a43488479eb77a8609e28adf3978d47f0c8f7a74
ChumboMJ <tforste@gmail.com>
2024-12-09 21:00:45 -0800

    Not functional - Fixed typo where y_coordinates was being assigned table["x-coordinate"], added some prints to help chase a bug

--- SecretMessageCodingChallenge.py
+++ SecretMessageCodingChallenge.py
@@ -34,11 +34,20 @@ def process_google_doc(url):
     table = df[0]
 
     x_coordinates = table["x-coordinate"]
-    y_coordinates = table["x-coordinate"]
+    y_coordinates = table["y-coordinate"]
+
+    # Printing Debugging Outputs
+    print("== x coordinates ==")
+    print(x_coordinates)
+
+    print("== y coordinates ==")
+    print(y_coordinates)
 
     # Determine Grid Size for output
     max_x = x_coordinates.max()
     max_y = y_coordinates.max()
+    #print(str(max_x))
+    #print(str(max_y))
 
     # Create an empty grid
     output_grid = [[' ' for _ in range(max_y + 1)] for _ in range(max_x + 1)]
@@ -46,7 +55,7 @@ def process_google_doc(url):
     # Place characters in the grid
     for index, row in table.iterrows():
         x, y, char = row["x-coordinate"], row["y-coordinate"], row["Character"]
-        output_grid[x][y] = char
+        output_grid[y][x] = char
 
     for row in output_grid:
         print(''.join(row))
@@ -55,4 +64,4 @@ def process_google_doc(url):
 simple_google_doc_url = "https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub"
 google_doc_url = "https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5ieJuYwpt7YHYEzeNJkIb9OSDdx-ov2nRNReKQyey-cwJOoEKUhLmN9z/pub"
 process_google_doc(simple_google_doc_url)
-# process_google_doc(google_doc_url)
+#process_google_doc(google_doc_url)
